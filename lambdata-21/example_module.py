cbade7026f1b111f5f1ea3c91cebb552243caf05
Parvi <sing.parvi1@gmail.com>
2021-03-10 21:39:35 -0500

    make it better

---
+++ lambdata-21/example_module.py
@@ -0,0 +1,18 @@
+"""Lambda - a collection of Data Science helper functions
+"""
+# accessing Libraries through pipenv
+
+import pandas as pd
+import numpy as np
+
+FAV_NUM = [1,2,3,4,5555,55243232]
+
+def df_cleaner(df):
+    """Clean a git
+
+    Args:
+        df ([type]): [description]
+    """
+    # TO DO 
+    
+    pas
